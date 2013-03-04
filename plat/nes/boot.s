45121f6d870e3e7b9bf4e64ecd2e69ddc8a617cc
Manoel Trapier <manoelt@bookeen.com>
2013-03-04 19:00:38 +0100

    Copy pc86 platform to nes platform, and make change accordingly.

---
+++ plat/nes/boot.s
@@ -0,0 +1,36 @@
+#
+! $Source$
+! $State$
+! $Revision$
+
+! Declare segments (the order is important).
+
+.sect .text
+.sect .rom
+.sect .data
+.sect .bss
+
+.sect .text
+
+
+start2:
+	jmp _main
+
+.define __exit
+.extern __exit
+.define EXIT
+.extern EXIT
+__exit:
+EXIT:
+
+.define begtext, begdata, begbss
+.sect .data;       begdata:
+.sect .rom;        begrom:
+.sect .bss;        begbss:
+
+! Some magic data. All EM systems need these.
+
+.define .trppc, .ignmask, _errno
+.comm .trppc, 4
+.comm .ignmask, 4
+.comm _errno, 4
